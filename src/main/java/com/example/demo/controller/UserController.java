6194f387945940350e30d5617ff411b56062941a
devc1234 <namdevchavan713@gmail.com>
2024-06-25 21:11:36 +0530

    model mapper implements

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,9 +3,11 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,7 @@ import com.example.demo.payloads.UserDto;
 import com.example.demo.servise.UserServises;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users/")
 public class UserController {
 	
 	
@@ -22,16 +24,26 @@ public class UserController {
 	private UserServises userServises;
 	
 	
-	@PostMapping("/adduser")
+	@PostMapping("/")
 	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
 		UserDto createUserDto = this.userServises.createUser(userDto);
 		return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
 	}
 	
-	@GetMapping("/getUsers")
+	@GetMapping("/")
 	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId){
 		return ResponseEntity.ok(this.userServises.getUserById(userId));
 		
 	}
-
+	
+	@PutMapping("/{userId}")
+	  ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("UserId") Integer userId){
+	UserDto updatUser = this.userServises.updateUser(userDto, userId);
+	return ResponseEntity.ok(updatUser);
+	}
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<? >deleteUser(@PathVariable("userId") Integer userId){
+		return this.deleteUser(userId);
+	}
 }
