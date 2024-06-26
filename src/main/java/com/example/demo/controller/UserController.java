c198627bef3dea8921c4779a2a2ab47f1d8bd0fc
devc1234 <namdevchavan713@gmail.com>
2024-06-26 19:43:20 +0530

    Hibernate validate provide and security add

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,34 +22,45 @@ import com.example.demo.payloads.UserDto;
 import com.example.demo.servise.UserServises;
 
 @RestController
-@RequestMapping("/users/")
+@RequestMapping("/api/v1/users")
 public class UserController {
-	
-	
+
 	@Autowired
-	private UserServises userServises;
-	
+	private UserServises userService;
+
 	
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
-		UserDto createUserDto = this.userServises.createUser(userDto);
-		return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
+		UserDto createUserDto = this.userService.createUser(userDto);
+		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
 	}
+
+
+
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
+		UserDto updatedUser = this.userService.updateUser(userDto, uid);
+		return ResponseEntity.ok(updatedUser);
+	}
+
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@DeleteMapping("/{userId}")
+	public <ApiResponse> ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid) {
+		this.userService.deleteUser(uid);
+		return new ResponseEntity<ApiResponse>(HttpStatus.OK);
+	}
+
 	
 	@GetMapping("/")
-	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId){
-		return ResponseEntity.ok(this.userServises.getUserById(userId));
-		
+	public ResponseEntity<List<UserDto>> getAllUsers() {
+		return ResponseEntity.ok(this.userService.getAllUser());
 	}
+
 	
-	@PutMapping("/{userId}")
-	  ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("UserId") Integer userId){
-	UserDto updatUser = this.userServises.updateUser(userDto, userId);
-	return ResponseEntity.ok(updatUser);
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId) {
+		return ResponseEntity.ok(this.userService.getUserById(userId));
 	}
-	
-	@DeleteMapping("/{userId}")
-	public ResponseEntity<? >deleteUser(@PathVariable("userId") Integer userId){
-		return this.deleteUser(userId);
-	}
+
 }
